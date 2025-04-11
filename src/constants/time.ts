8f6cb5e5271c096804dfc265a578bc1829bc320f
rrgoetz <ryan.r.goetz@jpl.nasa.gov>
2025-04-11 05:42:57 -1000

    Rename and added groups to regex

--- src/constants/time.ts
+++ src/constants/time.ts
@@ -1,5 +1,11 @@
-export const ABSOLUTE_TIME = /^(\d{4})-(\d{3})T(\d{2}):(\d{2}):(\d{2})(?:\.(\d{3}))?$/;
-export const RELATIVE_TIME =/^(?<doy>([0-9]{3}))?(T)?(?<hr>([0-9]{2})):(?<mins>([0-9]{2})):(?<secs>[0-9]{2})?(\.)?(?<ms>([0-9]+))?$/;
-export const RELATIVE_SIMPLE = /(\d+)(\.[0-9]+)?$/;
-export const EPOCH_TIME =/^((?<sign>[+-]?))(?<doy>([0-9]{3}))?(T)?(?<hr>([0-9]{2})):(?<mins>([0-9]{2})):(?<secs>[0-9]{2})?(\.)?(?<ms>([0-9]+))?$/;
-export const EPOCH_SIMPLE = /(^[+-]?)(\d+)(\.[0-9]+)?$/;
+export const ISO_ORDINAL_TIME_REGEX =
+  /^(?<year>(\d{4}))-(?<doy>(\d{3}))T(?<hr>(\d{2})):(?<mins>(\d{2})):(?<secs>(\d{2}))(?:\.(?<ms>\d+))?$/;
+export const ISO_8601_UTC_REGEX =
+  /^(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})T(?<hr>\d{2}):(?<mins>\d{2}):(?<secs>\d{2})(?:\.(?<ms>\d+))?Z$/;
+export const DOY_TIME_REGEX =
+  /^((?<sign>[+-]?))(?<doy>(\d{3}))?(T)?(?<hr>(\d{2})):(?<mins>(\d{2})):(?<secs>\d{2})(?:\.(?<ms>\d+))?$/;
+export const SECOND_TIME_REGEX =
+  /^(?<sign>([+-]?))(?<secs>(\d+))(?:\.(?<ms>\d+))?$/;
+
+export const MAX_UTC_YEAR = 9999;
+export const MAX_DOY = 365;
